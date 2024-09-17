cabb4a777d66ac0054f13006e6ce3a0376a862a7
Nicholas <142728088+Nichonicholass@users.noreply.github.com>
2024-09-17 19:57:30 +0700

    Create encapsulation.java

---
+++ encapsulation.java
@@ -0,0 +1,38 @@
+package encapsulation;
+class Person {
+    private String name; // Data tersembunyi
+    private int age;
+
+    // Setter untuk nama
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter untuk nama
+    public String getName() {
+        return name;
+    }
+
+    // Setter untuk usia
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        }
+    }
+
+    // Getter untuk usia
+    public int getAge() {
+        return age;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Adit");
+        person.setAge(30);
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());   
+    }
+}
