05548499bc7b275986c8d115dab09ede46058c77
Nicholas <142728088+Nichonicholass@users.noreply.github.com>
2024-09-17 21:01:13 +0700

    Create polymorphism.java

---
+++ polymorphism.java
@@ -0,0 +1,29 @@
+class Animal {
+    void makeSound() {
+        System.out.println("Some sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Woof");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Meow");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myDog.makeSound(); 
+        myCat.makeSound(); 
+    }
+}
